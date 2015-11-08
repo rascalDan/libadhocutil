c5628db861b753d1eeaf67c2f5c05f4f31d2dfda
Dan Goodliffe <dan@randomdan.homeip.net>
2015-11-08 21:54:52 +0000

    Add doxygen comments

--- libadhocutil/exception.h
+++ libadhocutil/exception.h
@@ -6,9 +6,11 @@
 #include <boost/optional.hpp>
 
 namespace AdHoc {
+	/// Helper class for lazy creation of exception message text.
 	template <typename BaseException>
 	class Exception : public BaseException {
 		public:
+			/// Override of std::exception::what() to create text as required.
 			inline const char * what() const throw() override
 			{
 				if (!msg) {
@@ -18,6 +20,7 @@ namespace AdHoc {
 			}
 
 		private:
+			/// Message text provider.
 			virtual std::string message() const throw() = 0;
 			mutable boost::optional<std::string> msg;
 	};
