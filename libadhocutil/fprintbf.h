49b20741fb018f5c54d905465ef84a059321d544
Dan Goodliffe <dan@randomdan.homeip.net>
2015-12-06 00:36:42 +0000

    Inline the fprintbf wrappers

--- libadhocutil/fprintbf.h
+++ libadhocutil/fprintbf.h
@@ -13,13 +13,13 @@ DLL_PUBLIC size_t fprintbf(FILE *, const boost::format &);
 DLL_PUBLIC FILE * fopen(const boost::filesystem::path & path, const char * mode);
 
 template <typename ... Params>
-size_t fprintbf(FILE * f, const std::string & fmt, const Params & ... p)
+size_t inline fprintbf(FILE * f, const std::string & fmt, const Params & ... p)
 {
 	return fprintbf(f, *AdHoc::Buffer::getFormat(fmt), p...);
 }
 
 template <typename Param, typename ... Params>
-size_t fprintbf(FILE * f, boost::format & fmt, const Param & p, const Params & ... ps)
+size_t inline fprintbf(FILE * f, boost::format & fmt, const Param & p, const Params & ... ps)
 {
 	fmt % p;
 	return fprintbf(f, fmt, ps...);
