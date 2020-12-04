9d03e663e2edf3c992dfba92f15ac597ca864530
Dan Goodliffe <dan@randomdan.homeip.net>
2020-12-04 19:51:29 +0000

    Remove copy initialisation of locks in ScopeLock

--- libadhocutil/lockHelpers.h
+++ libadhocutil/lockHelpers.h
@@ -6,7 +6,7 @@
 #define LIBADHOC_LOCK_WITHLINE(a) LIBADHOC_LOCK_CONCAT(a, __LINE__)
 
 #define BaseLock(l, lt) lt LIBADHOC_LOCK_WITHLINE(_lck)(l)
-#define BaseScopeLock(l, lt) if (auto LIBADHOC_LOCK_WITHLINE(_lck) = lt(l))
+#define BaseScopeLock(l, lt) if (lt LIBADHOC_LOCK_WITHLINE(_lck) {l})
 
 #define Lock(l) BaseLock(l, std::unique_lock)
 #define SharedLock(l) BaseLock(l, std::shared_lock)
