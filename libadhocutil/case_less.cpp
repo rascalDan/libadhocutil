54d94698a26a6e48e691fde17791b2f6e4ca73b7
Dan Goodliffe <dan@randomdan.homeip.net>
2021-10-16 12:28:09 +0100

    Fix static_assert on case_less function

--- libadhocutil/case_less.cpp
+++ libadhocutil/case_less.cpp
@@ -1,4 +1,5 @@
 #include "case_less.h"
 #include <string_view>
 
-static_assert(&AdHoc::case_less::operator()<std::string_view, std::string_view>);
+static_assert(
+		std::is_member_function_pointer_v<decltype(&AdHoc::case_less::operator()<std::string_view, std::string_view>)>);
