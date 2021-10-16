a703fae42ee392050e22e1c1bdfba8597ca1a5bc
Dan Goodliffe <dan@randomdan.homeip.net>
2021-10-16 12:55:28 +0100

    <type_traits> needed for std::is_member_function_pointer_v

--- libadhocutil/case_less.cpp
+++ libadhocutil/case_less.cpp
@@ -1,5 +1,6 @@
 #include "case_less.h"
 #include <string_view>
+#include <type_traits>
 
 static_assert(
 		std::is_member_function_pointer_v<decltype(&AdHoc::case_less::operator()<std::string_view, std::string_view>)>);
